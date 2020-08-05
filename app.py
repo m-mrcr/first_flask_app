038b3f1615ca235891264ce3a81ab6ff9371ccc5
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-08-05 16:27:20 -0600

    Add index method with movies array

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+movies = [
+  {
+      "name": "The Shawshank Redemption",
+      "casts": ["Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler"],
+      "genres": ["Drama"]
+  },
+  {
+      "name": "The Godfather ",
+      "casts": ["Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton"],
+      "genres": ["Crime", "Drama"]
+  }
+]
+
+@app.route('/movies')
 def index():
-  return {'hello': 'world'}
+  return jsonify(movies)
 
 app.run()
