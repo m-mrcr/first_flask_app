68cbc4fd998efca89802ffe21db7dfe8c57aba27
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-08-05 16:59:26 -0600

    Add post method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -18,5 +18,22 @@ movies = [
 @app.route('/movies')
 def index():
   return jsonify(movies)
+
+@app.route('/movies', methods=['POST'])
+def add_movie():
+  movie = request.get_json()
+  movies.append(movie)
+  return {'id': len(movies)-1}, 200
+
+@app.route('/movies/<int:index>', methods=['PUT'])
+def update_movie(index):
+  movie = request.get_json()
+  movies[index] = movie
+  return jsonify(movies[index]), 200
+
+@app.route('/movies/<int:index>', methods=['DELETE'])
+def delete_movie(index):
+  movies.pop(index)
+  return 'None', 200  
 
 app.run()
