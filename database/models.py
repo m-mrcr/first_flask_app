2460463d56e2e000aabc61797bbeebd30b014a2f
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2020-08-06 16:54:20 -0600

    Iteration 2

--- database/models.py
+++ database/models.py
@@ -1,7 +1,7 @@
 from .db import db
 
 class Movie(db.Document):
-  name = db.StringField(required=True, unique=True)
-  casts = db.ListField(db.StringField(), required=True)
-  genres = db.ListField(db.StringField(), required=True)
-  
+    name = db.StringField(required=True, unique=True)
+    casts = db.ListField(db.StringField(), required=True)
+    genres = db.ListField(db.StringField(), required=True)
+        
